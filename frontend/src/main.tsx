d9830d4c9098a128cd8e890036718d3971efdae1
VinnRe <22-03804@g.batstate-u.edu.ph>
2025-05-02 10:32:51 +0800

    Improve react routers and routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import LoginPage from './pages/Login.tsx'
+import InventoryPage from './pages/Inventory.tsx'
+import NotFoundPage from './pages/NotFoundPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>HOME</div>,
+    errorElement: <NotFoundPage />
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/inventory',
+    element: <InventoryPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
